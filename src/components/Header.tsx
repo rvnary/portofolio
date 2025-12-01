fefbd89e490e8777e481d7cc6b94b7a58f385df2
rvn <135384767+rvnary@users.noreply.github.com>
2025-12-01 22:09:38 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
           {/* Logo */}
           <div className="flex items-center gap-2">
             <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
-              <img src="g_logo.svg" alt="logo"></img>
+              <img src="https://raw.githubusercontent.com/rvnary/Portofolio/refs/heads/main/justintano/g_logo.svg" alt="logo"></img>
             </div>
           </div>
 
